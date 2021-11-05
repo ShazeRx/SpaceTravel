1411f3877cabe5e925553b6cfdb63780de9044a6
perkowskikrzysio@gmail.com <perkowskikrzysio@gmail.com>
2021-11-05 13:03:26 +0100

    Added models, user managers + tests, added pytest + conf

--- spacetravel_app/admin.py
+++ spacetravel_app/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from spacetravel_app.models import MedicalExamination, Shuttle, SpaceStation, Offer, Reservation, CustomUser
+
+admin.site.register(MedicalExamination)
+admin.site.register(Shuttle)
+admin.site.register(SpaceStation)
+admin.site.register(Offer)
+admin.site.register(Reservation)
+admin.site.register(CustomUser)
--- spacetravel_app/tests.py
+++ spacetravel_app/tests.py
@@ -1,1 +1,33 @@
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from unittest import TestCase
+import pytest
+
+
+class CustomUserManagerTests(TestCase):
+
+    @pytest.mark.django_db
+    def test_should_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(email='test@example.com', password='fizz', date_of_birth='1999-06-29')
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+
+    @pytest.mark.django_db
+    def test_should_raise_type_error_when_empty_or_wrong_fields(self):
+        User = get_user_model()
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', password='fizz', date_of_birth='1999-06-29')
+            User.objects.create_user(email='test@example.com', password='', date_of_birth='1999-06-29')
+            User.objects.create_user()
+            User.objects.create_user(email='test@example.com', password='foo', date_of_birth='')
+
+    @pytest.mark.django_db
+    def test_should_create_superuser(self):
+        User = get_user_model()
+        superuser = User.objects.create_superuser(email='test@example.com', password='fizz')
+        self.assertEqual(superuser.email, 'test@example.com')
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
